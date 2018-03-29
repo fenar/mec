4d1dfcd042ef3e0cd5afafc5743458b5694d78b3
Samuel Cozannet <samuel.cozannet@madeden.com>
2018-03-29 13:30:01 -0500

    Updates

--- 00-juju-controller-bootstrap.sh
+++ 00-juju-controller-bootstrap.sh
@@ -2,6 +2,6 @@
 #Author: Fatih E. NAR
 set -eaux
 obnum=`hostname | cut -c 10- -`
-time juju bootstrap --config bootstrap-timeout=1200 --to Node01CPU01-VM0.maas --show-log v4n${obnum}-maas  maas-v4n${obnum}-controller
+time juju bootstrap --config bootstrap-timeout=1200 --to v4n-node01ob160.maas --show-log v4n${obnum}-maas  maas-v4n${obnum}-controller
 echo "..."
 juju gui --no-browser --show-credentials
